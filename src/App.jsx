df6e540213fd014a4b8a3a74d2db83ddfad23723
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-09-11 10:10:04 -0700

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AppLayout from "./layouts/AppLayout";
-import HomePage from "./pages/HomePage";
-
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/random" element={<p>Random</p>} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <div>nugo</div>;
 };
 
 export default App;
