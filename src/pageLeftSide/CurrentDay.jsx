0bb5de5ba30ee4c69e7ddbeb1f35258738fb4935
marriamdd <davitashvili.m3@gmail.com>
2024-09-12 20:03:52 +0400

    reaftor:get current location

--- src/pageLeftSide/CurrentDay.jsx
+++ src/pageLeftSide/CurrentDay.jsx
@@ -1,15 +1,4 @@
-// import ContentContainerShape from "./ContentContainerShape";
-
 export default function CurrentDay() {
-  // const currentDayData = {
-  //   label: { label: "Batumi", text: "40px", font: "510" },
-  //   currentDay: { currentDay: "14 sec, Monday", text: "24px", font: "400" },
-  //   humidity: { humidity: "Sunny", text: "24px", font: "400" },
-  //   tempricha: { tempricha: "31°", text: "72px", font: "400" },
-  //   paddingX: "52px",
-  //   paddingY: "35px",
-  // };
-
   return (
     <div className="bg-[#B5B5B566] text-[#FFFFFF] px-[52px] py-[35px] rounded-[8px] ">
       <div>
--- src/pageLeftSide/LeftSide.jsx
+++ src/pageLeftSide/LeftSide.jsx
@@ -4,28 +4,52 @@ import Humidity from "./Humidity";
 import Sensitivity from "./Sensitivity";
 import TemprichaByHours from "./TemprichaByHours";
 import { hourlyData } from "./hourlyData";
-import CurrentLocation from "./CurentLocation";
+import { useCurrentLocation } from "./customHooks/GetCurrentLocation";
 
 export default function LeftSide() {
   const { t } = useTranslation();
+  const { address, fetchLocationDetails, error, loading } =
+    useCurrentLocation();
+
   return (
-    <div className=" w-[860px]  pt-[12px] pb-[30px] px-[2rem]">
-      <h2 className="text-[28px]  text-[#FFFFFF] pt-[38px] pb-[27px] font-[510]">
+    <div className="w-[860px] pt-[12px] pb-[30px] px-[2rem]">
+      <h2 className="text-[28px] text-[#FFFFFF] pt-[38px] pb-[27px] font-[510]">
         {t("Forecast of the day")}
       </h2>
       <CurrentDay />
-      <h4 className="underline text-[#FFFFFF] pt-[14px] pb-[45px] text-[16px] font-[400]">
+
+      <h4
+        onClick={fetchLocationDetails}
+        className="underline text-[#FFFFFF] pt-[14px] pb-[45px] text-[16px] font-[400] cursor-pointer"
+      >
         {t("current location")}
       </h4>
+
+      {loading && <p className="text-[#FFFFFF]">Fetching location...</p>}
+      {error && (
+        <p className="text-[#FF0000]">
+          {t("Error fetching location")}: {error}
+        </p>
+      )}
+      {error === "User denied Geolocation" && (
+        <p className="text-[#FF0000]">
+          {t(
+            "Please allow location access or enable it in your browser settings."
+          )}
+        </p>
+      )}
+      {address && <p className="text-[#FFFFFF]">{address}</p>}
+
       <h3 className="text-[24px] text-[#FFFFFF] font-[510] pt-[45px] pb-[29px]">
         {t("weather by hours")}
       </h3>
+
       <TemprichaByHours hourlyData={hourlyData} />
+
       <div className="flex w-full justify-between">
         <Humidity />
         <Sensitivity />
       </div>
-      <CurrentLocation />
     </div>
   );
 }
